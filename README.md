07fed584ee35389e77c7b249004f5242be33bb5e
LegnarMaximilian <maximilian.legnar@uni-heidelberg.de>
2025-03-06 15:39:59 +0100

    added extract_macro_images.py

--- README.md
+++ README.md
@@ -70,14 +70,15 @@ WSI Babelfish needs to know which information (staining-letters, block-num, etc.
 
 For this, WSI-BabelFish can load ROI-configuration files. (ROI = Region of Interest).
 
-To create a new ROI-configuration file, one can use the scripts located in `./tools_ROIconfig` and some open source tools:
+To create a new ROI-configuration file, please follow these steps:
 
 1. Download and install [ImageJ](https://imagej.net/ij/).
 2. Get a macro-image (png or jpg) of one of your slide-files. To extract a macro-image from a WSI, you can use the `extract_macro_image.py` script in `./tools_ROIconfig`.
 3. Open the macro-image in ImageJ.
 4. Use the `Rectangle`-Tool to draw ROI's. Add each rectangle to a ROI-set using right-mouse click -> `add to ROI-Manager` and name each ROI accordingly to what information is located in this region.
-5. In the ROI manager, select all ROIs and export them as a RoiSet zip file and copy it into the `./tools_ROIconfig` directory.
-6. todo... implement ROI-configurations...
+5. In the ROI manager, select all ROIs and export them as one RoiSet zip file.
+
+... todo ...
 
 #### How to adapt the OCR tools:
 
@@ -113,7 +114,7 @@ python -m pip install rapidfuzz
 python -m pip install thefuzz
 python -m pip install tqdm
 python -m pip install icecream
-
+python -m pip install roifile
 ```
 
 ### Install openslide:
--- tools_ROIconfig/extract_macro_images.py
+++ tools_ROIconfig/extract_macro_images.py
@@ -1,1 +1,49 @@
-# todo...
+import os, sys
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description='Process some arguments.')
+    parser.add_argument('--openslide_dll', type=str, required=False, default=None, help='Path to the OpenSlide DLL directory')
+    parser.add_argument('--in_folder', type=str, required=True, help='Input folder containing WSI files')
+    parser.add_argument('--file_type', type=str, required=True, help='File type postfix to filter WSI files')
+
+    args = parser.parse_args()
+
+    if hasattr(os, 'add_dll_directory'):  # Windows
+        try:
+            with os.add_dll_directory(args.openslide_dll):
+                import openslide
+        except:
+            print(f"WARNING: Failed to add necessary dll directory for openbslide!\n"
+                  f"On windows, you have to pass openslide dll directory as argument --openslide_dll. "
+                  f"This may cause a crash!")
+            import openslide
+    else:
+        import openslide
+
+    try:
+        in_folder = args.in_folder
+        file_name_postfixes = [args.file_type] if ',' not in args.file_type else args.file_type.split(',')
+    except Exception as e:
+        raise ValueError(f"Failed to parse arguments: {e}. \nPlease provide the input folder as first argument and the "
+                         f"file name postfixes as the rest of the arguments.")
+
+    for wsi_file_name in os.listdir(in_folder):
+
+        if any([not wsi_file_name.endswith(file_name_postfix) for file_name_postfix in file_name_postfixes]):
+            continue
+
+        wsi_file_path = os.path.join(in_folder, wsi_file_name)
+
+        file_type = '.' + wsi_file_name.split('.')[-1]
+        try:
+            wsi = openslide.OpenSlide(wsi_file_path)
+        except Exception as e:
+            print(f"Failed to load {wsi_file_path} due to loading error: {e}")
+        macro_img = wsi.associated_images['macro']
+        out_path = wsi_file_path.replace(file_type, '') + "_macro.png"
+        macro_img.save(out_path)
+        print(f"Extracted \t{out_path}")
+
+if __name__ == "__main__":
+    main()
